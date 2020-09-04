f4081f5dee589d2af59a465705596f2ba8359ab8
Wha-The <47490607+Wha-The@users.noreply.github.com>
2020-09-04 10:40:53 +0800

    Update remote_desktop.py

--- remote_desktop.py
+++ remote_desktop.py
@@ -1,8 +1,29 @@
-import sys,subprocess
+import sys,subprocess,os,zipfile
 importSuccess = False
+ignoreUpdates=False
+latestzip="lastest.zip"
+
 class FatalError(Exception):pass
 def install_package(package):return subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 specialpackagenames = {"wx":"wxpython","pil":"pillow",}
+try:
+	import requests
+except ImportError:
+	install_package("requests")
+print("Checking for updates...")
+data = requests.get("https://github.com/Wha-The/remote-desktop/archive/master.zip").content
+lastest_update = os.path.exists(latestzip)and open(latestzip,'rb')
+if data!=lastest_update:
+	chooseToUpdate=not ignoreUpdates and raw_input("Update found, would you like to install it? (y/n)").lower()=="y"
+	if chooseToUpdate:
+		open(lastestzip,'wb').write(data)
+		with zipfile.ZipFile(lastestzip, 'r') as zip_ref:
+			zip_ref.extractall(".")
+		print("Updated! ")
+		os.execl(sys.executable, os.path.abspath(__file__), *sys.argv) 
+		quit()
+
+
 while not importSuccess:
 	try:import Server,Client
 	except ImportError as e:
